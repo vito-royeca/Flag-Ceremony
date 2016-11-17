f5616017c7edfb17cb1f54b777c0c88ce80c1ddd
jovito <jovit.royeca@gmail.com>
2016-11-17 14:44:09 +0800

    feat: added Firebase/Database pod.

---
+++ Flag Ceremony/Capital.swift
@@ -0,0 +1,9 @@
+//
+//  Capital.swift
+//  Flag Ceremony
+//
+//  Created by Jovit Royeca on 17/11/2016.
+//  Copyright © 2016 Jovit Royeca. All rights reserved.
+//
+
+import Foundation
---
+++ Flag Ceremony/Country.swift
@@ -0,0 +1,9 @@
+//
+//  Country.swift
+//  Flag Ceremony
+//
+//  Created by Jovit Royeca on 17/11/2016.
+//  Copyright © 2016 Jovit Royeca. All rights reserved.
+//
+
+import Foundation
