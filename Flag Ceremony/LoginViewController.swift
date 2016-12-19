dd9c1ca97da72820ee767ea69bbed408276280b0
Jovito Royeca <jovitoroyeca@Jovitos-MBP.fritz.box>
2016-12-19 02:07:56 +0100

    feat: Added Faceboo SDK pods

---
+++ Flag Ceremony/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Flag Ceremony
+//
+//  Created by Jovito Royeca on 18/12/2016.
+//  Copyright © 2016 Jovit Royeca. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
