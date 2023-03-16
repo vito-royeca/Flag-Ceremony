9b0e500d163228de71e57103b6aa8d2dcd10fd68
Vito Royeca <vito.royeca@gmail.com>
2023-03-16 09:17:03 -0400

    More refactors

---
+++ Shared/Views/CountryView.swift
@@ -0,0 +1,20 @@
+//
+//  CountryView.swift
+//  Flag Ceremony
+//
+//  Created by Vito Royeca on 3/16/23.
+//
+
+import SwiftUI
+
+struct CountryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryView()
+    }
+}
