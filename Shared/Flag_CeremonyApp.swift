5f18fee620bae8c44651feb84674b9d7560389ee
Vito Royeca <vito.royeca@gmail.com>
2023-04-20 21:19:38 -0400

    Removed Persistence

--- Shared/Flag_CeremonyApp.swift
+++ Shared/Flag_CeremonyApp.swift
@@ -12,7 +12,6 @@ import GoogleSignIn
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print("Flag Ceremony application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
         FirebaseApp.configure()
         return true
     }
@@ -31,12 +30,9 @@ struct Flag_CeremonyApp: App {
     // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            TabNavigationView()
         }
     }
 }
